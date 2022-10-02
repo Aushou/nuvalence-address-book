5002bd03f63d56ba87b29401e23097a87096fb47
Christian Gonzalez <aushou@me.com>
2022-10-02 17:31:48 -0400

    test(contact): update service tests

--- src/app/contact.service.spec.ts
+++ src/app/contact.service.spec.ts
@@ -33,7 +33,7 @@ describe('ContactService', () => {
         expect(contacts.length).toBe(5);
         expect(contacts).toEqual(mockContacts.results)
       });
-      const req = httpTestingController.expectOne('https://randomuser.me/api/?results=100&seed=nuvalence');
+      const req = httpTestingController.expectOne('https://randomuser.me/api/?results=100&seed=nuvalence&inc=name,picture,phone,cell,login');
       expect(req.request.method).toBe('GET');
       req.flush(mockContacts);
     });
@@ -42,7 +42,7 @@ describe('ContactService', () => {
       service.getContacts().subscribe(contacts => {
         expect(contacts.length).toBe(0);
       });
-      const req = httpTestingController.expectOne('https://randomuser.me/api/?results=100&seed=nuvalence');
+      const req = httpTestingController.expectOne('https://randomuser.me/api/?results=100&seed=nuvalence&inc=name,picture,phone,cell,login');
       req.flush('404 Not Found', { status: 404, statusText: 'Not Found' });
     })
   });
@@ -55,7 +55,7 @@ describe('ContactService', () => {
         expect(contacts).toEqual(mockContacts.results)
       });
       const req = httpTestingController
-        .expectOne('https://randomuser.me/api/?seed=nuvalence&page=1&results=5');
+        .expectOne('https://randomuser.me/api/?seed=nuvalence&inc=name,picture,phone,cell,login&page=1&results=5');
       expect(req.request.method).toBe('GET');
       req.flush(mockContacts);
     });
@@ -70,7 +70,7 @@ describe('ContactService', () => {
           expect(contacts).toEqual(mockContacts.results)
         });
       const req = httpTestingController
-        .expectOne(`https://randomuser.me/api/?seed=nuvalence&page=${givenPage+1}&results=${givenSize}`);
+        .expectOne(`https://randomuser.me/api/?seed=nuvalence&inc=name,picture,phone,cell,login&page=${givenPage+1}&results=${givenSize}`);
       expect(req.request.method).toBe('GET');
       req.flush(mockContacts);
     })
@@ -80,7 +80,7 @@ describe('ContactService', () => {
         expect(contacts.length).toBe(0);
       });
       const req = httpTestingController
-        .expectOne('https://randomuser.me/api/?seed=nuvalence&page=1&results=5');
+        .expectOne('https://randomuser.me/api/?seed=nuvalence&inc=name,picture,phone,cell,login&page=1&results=5');
       req.flush('404 Not Found', { status: 404, statusText: 'Not Found' });
     })
   });
@@ -93,7 +93,7 @@ describe('ContactService', () => {
         expect(contact).toBeDefined();
         expect(contact).toEqual(mockContacts.results[0])
       });
-      const req = httpTestingController.expectOne('https://randomuser.me/api/?results=100&seed=nuvalence');
+      const req = httpTestingController.expectOne('https://randomuser.me/api/?results=100&seed=nuvalence&inc=name,picture,phone,cell,login');
       expect(req.request.method).toBe('GET');
       req.flush(mockContacts);
     });
