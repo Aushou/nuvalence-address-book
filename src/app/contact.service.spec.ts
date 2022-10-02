266896a3400ddaaafd1457785b074fc6560ff09c
Christian Gonzalez <aushou@me.com>
2022-10-02 16:22:00 -0400

    test(service): test paginated function

--- src/app/contact.service.spec.ts
+++ src/app/contact.service.spec.ts
@@ -47,6 +47,45 @@ describe('ContactService', () => {
     })
   });
 
+  describe('Get Contacts Paginated', () => {
+    it('should return an Observable of a list of contacts on success', () => {
+      const mockContacts: ContactResponse = CONTACTS
+      service.getContactsPaginated(0, 5).subscribe(contacts => {
+        expect(contacts.length).toBe(5);
+        expect(contacts).toEqual(mockContacts.results)
+      });
+      const req = httpTestingController
+        .expectOne('https://randomuser.me/api/?seed=nuvalence&page=1&results=5');
+      expect(req.request.method).toBe('GET');
+      req.flush(mockContacts);
+    });
+
+    it('should create the correct query params from the input params', () => {
+      const mockContacts: ContactResponse = CONTACTS
+      const givenPage = 3;
+      const givenSize = 10;
+      service.getContactsPaginated(givenPage, givenSize)
+        .subscribe(contacts => {
+          expect(contacts.length).toBe(5);
+          expect(contacts).toEqual(mockContacts.results)
+        });
+      const req = httpTestingController
+        .expectOne(`https://randomuser.me/api/?seed=nuvalence&page=${givenPage+1}&results=${givenSize}`);
+      expect(req.request.method).toBe('GET');
+      req.flush(mockContacts);
+    })
+
+    it('should return an empty array on failure', () => {
+      service.getContactsPaginated(0, 5).subscribe(contacts => {
+        expect(contacts.length).toBe(0);
+      });
+      const req = httpTestingController
+        .expectOne('https://randomuser.me/api/?seed=nuvalence&page=1&results=5');
+      req.flush('404 Not Found', { status: 404, statusText: 'Not Found' });
+    })
+  });
+
+
   describe('Get Contact', () => {
     it('should return an Observable of a list of contacts on success', () => {
       const mockContacts: ContactResponse = CONTACTS
