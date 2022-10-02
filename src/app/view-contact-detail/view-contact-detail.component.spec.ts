37e9316542c3519a41ac958bcb8597fed41755dd
Christian Gonzalez <aushou@me.com>
2022-10-02 15:25:17 -0400

    test(contact): improve contact detail tests

--- src/app/view-contact-detail/view-contact-detail.component.spec.ts
+++ src/app/view-contact-detail/view-contact-detail.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
+import { EMPTY, of } from 'rxjs';
 import { ContactService } from '../contact.service';
 import { CONTACTS } from '../mocks/contacts.mock';
 
@@ -9,11 +10,14 @@ import { ViewContactDetailComponent } from './view-contact-detail.component';
 describe('ViewContactDetailComponent', () => {
   let component: ViewContactDetailComponent;
   let fixture: ComponentFixture<ViewContactDetailComponent>;
-
+  let mockContactService: any;
+  
   beforeEach(async () => {
+    mockContactService = jasmine.createSpyObj('ContactService', ['getContact']);
+
     await TestBed.configureTestingModule({
       providers: [
-        { provide: ContactService, useClass: MockContactService },
+        { provide: ContactService, useValue: mockContactService },
         { provide: ActivatedRoute, useClass: MockRoute}
       ],
       declarations: [ ViewContactDetailComponent ]
@@ -22,19 +26,28 @@ describe('ViewContactDetailComponent', () => {
 
     fixture = TestBed.createComponent(ViewContactDetailComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-});
 
-class MockContactService {
-  getContact(id: string) {
-    return of(CONTACTS.results[0]);
-  }
-}
+  it('should display the user\'s name from getContact', () => {
+    mockContactService.getContact.and.returnValue(of(CONTACTS.results[0]));
+    fixture.detectChanges();
+
+    const loadingElement = fixture.debugElement.query(By.css('h2')).nativeElement;
+    expect(loadingElement.innerHTML).toContain('Stanimir');
+  })
+
+  it('should display a placeholder when there is no result yet', () => {
+    mockContactService.getContact.and.returnValue(EMPTY);
+    fixture.detectChanges();
+    
+    const loadingElement = fixture.debugElement.query(By.css('div')).nativeElement;
+    expect(loadingElement.innerHTML).toContain('Loading');
+  });
+});
 
 class MockRoute {
   snapshot = {
